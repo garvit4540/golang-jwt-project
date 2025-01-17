79bacd7b920691f51577a90d076458a771020b7f
Garvit Gupta <garvitg02@gmail.com>
2025-01-17 23:14:37 +0530

    completed video 2 and moving to video 3

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,10 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(ctx *gin.Engine) {
+	ctx.POST("users/signup", controllers.Signup())
+	ctx.POST("users/login", controllers.Login())
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,7 +1,10 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func UserRoutes(ctx *gin.Engine) {
-
+	ctx.POST("users/users", controllers.GetUsers())
+	ctx.POST("users/:user_id", controllers.GetUser())
 }
