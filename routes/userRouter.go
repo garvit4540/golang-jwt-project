3df81b399f277ba34773e4145f4e9ff0763d9cd2
Garvit Gupta <garvitg02@gmail.com>
2025-01-19 19:11:07 +0530

    done and dusted

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -2,10 +2,12 @@ package routes
 
 import (
 	"github.com/garvit4540/golang-jwt-project/controllers"
+	"github.com/garvit4540/golang-jwt-project/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(ctx *gin.Engine) {
-	ctx.POST("users/users", controllers.GetUsers())
-	ctx.POST("users/:user_id", controllers.GetUser())
+	ctx.Use(middleware.Authenticate())
+	ctx.GET("users", controllers.GetUsers())
+	ctx.GET("users/:user_id", controllers.GetUser())
 }
